f469c303b6593c31e0923adc3e1fa39722b2e3dc
Aviral <21f1000608@student.onlinedegree.iitm.ac.in>
2022-08-23 17:31:57 +0530

    LoginWithUNSD

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { ConnectButton } from "web3uikit";
+import UDomain from "../components/UDomain";
 
 export default function Header() {
     return (
@@ -38,7 +39,8 @@ export default function Header() {
                     </h4>
                 </Link>
             </div>
-            <div className="ml-auto py-2 px-4">
+            <div className="flex ml-auto py-2 px-4">
+                <UDomain />
                 <ConnectButton moralisAuth={false} />
             </div>
         </nav>
